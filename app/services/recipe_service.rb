340236778f70d82abb6e39c375fa61d0188fc299
pcbennett108 <pcbennett42@gmail.com>
2023-10-31 22:36:33 -0600

    Add Faraday connection for spoonacular complex search

---
+++ app/services/recipe_service.rb
@@ -0,0 +1,13 @@
+class RecipeService
+
+  def get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new(url: 'https://api.spoonacular.com/recipes/complexSearch') do |faraday|
+      faraday.params['apiKey'] = Rails.application.credentials.spoonacular[:key]
+    end
+  end
+end
