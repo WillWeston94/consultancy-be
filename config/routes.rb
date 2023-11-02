deb01282772d2864d9297600b4e9f4d86f5c5828
pcbennett108 <pcbennett42@gmail.com>
2023-11-02 14:50:32 -0600

    Add show to routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   namespace :api do 
     namespace :v1 do 
-      resources :search, only: [:index]
+      resources :search, only: [:index, :show]
     end
   end
 end
